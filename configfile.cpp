b12a894a0224fad00a96085d750eaf1141c692cb
Hive <anon@example.com>
2023-12-09 01:58:39 +0100

    return default if config value is empty / do not store empty config values

--- configfile.cpp
+++ configfile.cpp
@@ -19,11 +19,21 @@ String ConfigFile::get(const char* _key, const char* _default)
         result += (char)f.read();
 
     f.close();
+
+	if(result.length() == 0)
+		return _default;
+
     return result;
 }
 
 bool ConfigFile::set(const char* _key, const char* _value)
 {
+	if(strlen(_key) == 0)
+	{
+		LittleFS.remove(_key);
+		return true;
+	}
+
     File f = LittleFS.open(_key, "w");
     if (!f)
     {
